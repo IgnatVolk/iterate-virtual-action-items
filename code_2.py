4fc9d553008c6c731cf7ac78b9176e553c3fdc2b
IgnatVolk <142455386+IgnatVolk@users.noreply.github.com>
2023-10-27 12:09:46 +0300

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
+    return data
+
+    for item in data:
+import random
+    data = [random.randint(1, 100) for _ in range(10)]
+    main()
+def main():
+
+def generate_random_data():
+
+
+    data = generate_random_data()
